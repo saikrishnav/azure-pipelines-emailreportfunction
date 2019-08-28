deb90d42c36271cfe06032645df78835f4571c2a
SaiKrishnaV <svajjala@microsoft.com>
2019-08-28 11:23:52 -0700

    Add Get, Post

--- EmailReportTrigger/index.ts
+++ EmailReportTrigger/index.ts
@@ -1,4 +1,4 @@
-import { AzureFunction, Context, HttpRequest, HttpMethod } from "@azure/functions";
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import * as KeyVault from "@azure/keyvault-secrets";
 import * as Identity from "@azure/identity";
 import { EmailSender } from "azure-devops-emailreporttask/EmailSender";
@@ -11,43 +11,54 @@ import { JsonConfigProvider } from "./JsonConfigProvider";
 import { SmtpConfiguration } from "azure-devops-emailreporttask/config/mail/SmtpConfiguration";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
+  context.log('HTTP trigger function processed a request.');
 
-    var body = "Sends EmailReport based on pipeline data.";
-    if (req.method == "POST") {
-        const vaultName = req.body.SmtpConnectionInfo.KeyVaultName;
-        const secretName = req.body.SmtpConnectionInfo.SecretName;
-        const userName = req.body.SmtpConnectionInfo.UserName;
-
-        const vaultUri = `https://${vaultName}.vault.azure.net/`;
-        let credentials = new Identity.EnvironmentCredential();
-        const keyVaultClient = new KeyVault.SecretsClient(vaultUri, credentials);
-
-        // We're setting the Secret value here and retrieving the secret value
-        const secretBundle = await keyVaultClient.getSecret(secretName);
-
-        const smtpHost = req.body.SmtpConnectionInfo.SmtpHost;
-        const password = secretBundle.value;
-        const enableSSLOnSmtpConnection = false;
-
-        const smtpConfig = new SmtpConfiguration(userName, password, smtpHost, enableSSLOnSmtpConnection);
-
-        const configProvider = new JsonConfigProvider(req.body, smtpConfig);
-        const reportConfiguration = new ReportConfiguration(configProvider);
-        const reportProvider = new ReportProvider(new DataProviderFactory(configProvider.getPipelineConfiguration()));
-
-        const reportManager = new ReportManager(
-            reportProvider,
-            new HTMLReportCreator(),
-            new EmailSender());
-
-        await reportManager.sendReportAsync(reportConfiguration);
-        body = "Mail Sent";
-    }
+  var body = "Sends EmailReport based on pipeline data.";
+  if (req.method == "GET") {
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: body
+      // status: 200, /* Defaults to 200 */
+      body: body
     };
+  } else if (req.method == "POST") {
+    body = `Starting Email Report Generation and condition evaluation based on request: ${req.body}`;
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body: body
+    };
+    const vaultName = req.body.SmtpConnectionInfo.KeyVaultName;
+    const secretName = req.body.SmtpConnectionInfo.SecretName;
+    const userName = req.body.SmtpConnectionInfo.UserName;
+
+    const vaultUri = `https://${vaultName}.vault.azure.net/`;
+    let credentials = new Identity.EnvironmentCredential();
+    const keyVaultClient = new KeyVault.SecretsClient(vaultUri, credentials);
+
+    // We're setting the Secret value here and retrieving the secret value
+    const secretBundle = await keyVaultClient.getSecret(secretName);
+
+    const smtpHost = req.body.SmtpConnectionInfo.SmtpHost;
+    const password = secretBundle.value;
+    const enableSSLOnSmtpConnection = false;
+
+    const smtpConfig = new SmtpConfiguration(userName, password, smtpHost, enableSSLOnSmtpConnection);
+
+    const configProvider = new JsonConfigProvider(req.body, smtpConfig);
+    const reportConfiguration = new ReportConfiguration(configProvider);
+    const reportProvider = new ReportProvider(new DataProviderFactory(configProvider.getPipelineConfiguration()));
+
+    const reportManager = new ReportManager(
+      reportProvider,
+      new HTMLReportCreator(),
+      new EmailSender());
+
+    reportManager.sendReportAsync(reportConfiguration);
+  } else {
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body: "Unsupported HttpMethod"
+    };
+  }
+
 };
 
 export default httpTrigger;
